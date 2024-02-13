bc1b5466953a27ece1580f0d7c3e769101252bfc
youta <m.youta.kwg@gmail.com>
2024-02-14 01:07:00 +0900

    (step1)ダミーデバイスの実装

--- driver/dummy.c
+++ driver/dummy.c
@@ -10,8 +10,17 @@
 static int
 dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
 {
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
+    /* drop data(データを破棄) */
+    return 0;
 }
 
+/**
+ * ダミーデバイスの仕様
+ * 入力: なし(データを受信することはない)
+ * 出力: データを破棄
+ */
 static struct net_device_ops dummy_ops = {
     .transmit = dummy_transmit,
 };
@@ -19,4 +28,24 @@ static struct net_device_ops dummy_ops = {
 struct net_device *
 dummy_init(void)
 {
+    struct net_device *dev;
+
+    dev = net_device_alloc();
+    if (!dev)
+    {
+        errorf("net_device_alloc() failure");
+        return NULL;
+    }
+    dev->type = NET_DEVICE_TYPE_DUMMY;
+    dev->mtu = DUMMY_MTU;
+    dev->hlen = 0;         /* non header */
+    dev->alen = 0;         /* non address */
+    dev->ops = &dummy_ops; /* デバイスドライバが実装している関数のアドレスを保持する構造体へのポインタを設定する */
+    if (net_device_register(dev) == -1)
+    {
+        errorf("net_device_register() failure");
+        return NULL;
+    }
+    debugf("initialized, dev=%s", dev->name);
+    return dev;
 }
