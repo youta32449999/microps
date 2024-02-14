6ba8382eba0018850dbe79ffe78a33494b2067c8
youta <m.youta.kwg@gmail.com>
2024-02-15 02:24:48 +0900

    (step2)ダミーデバイスを割り込みに対応させる

--- driver/dummy.c
+++ driver/dummy.c
@@ -17,12 +17,15 @@ dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_
     debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
     debugdump(data, len);
     /* drop data(データを破棄) */
+    intr_raise_irq(DUMMY_IRQ); /* テスト用に割り込みを発生させる */
     return 0;
 }
 
 static int
 dummy_isr(unsigned int irq, void *id)
 {
+    debugf("irq=%u, dev=%s", irq, ((struct net_device *)id)->name);
+    return 0;
 }
 
 /**
@@ -55,6 +58,8 @@ dummy_init(void)
         errorf("net_device_register() failure");
         return NULL;
     }
+    /* 割り込みハンドラとしてdummy_isrを登録する */
+    intr_request_irq(DUMMY_IRQ, dummy_isr, INTR_IRQ_SHARED, dev->name, dev);
     debugf("initialized, dev=%s", dev->name);
     return dev;
 }
