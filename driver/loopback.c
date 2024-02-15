246e90d182d87559d6b462c5612dc09e2126c538
youta <m.youta.kwg@gmail.com>
2024-02-15 23:52:52 +0900

    (step3)ループバックデバイスの初期化関数を実装

--- driver/loopback.c
+++ driver/loopback.c
@@ -14,6 +14,7 @@
 
 #define PRIV(x) ((struct loopback *)x->priv)
 
+/* ループバックデバイスのドライバ内で使用するプライベートなデータを格納するための構造体 */
 struct loopback
 {
     int irq;
@@ -45,4 +46,44 @@ static struct net_device_ops loopback_ops = {
 struct net_device *
 loopback_init(void)
 {
+    struct net_device *dev;
+    struct loopback *lo;
+
+    /* デバイスの生成とパラメータの設定 */
+    dev = net_device_alloc();
+    if (!dev)
+    {
+        errorf("net_device_alloc() failure");
+        return NULL;
+    }
+    dev->type = NET_DEVICE_TYPE_LOOPBACK;
+    dev->mtu = LOOPBACK_MTU;
+    dev->hlen = 0; /* non header */
+    dev->alen = 0; /* non address */
+    dev->flags = NET_DEVICE_FLAG_LOOPBACK;
+    dev->ops = &loopback_ops; /* デバイスドライバが実装している関数のアドレスを保持する構造体へのポインタを設定する */
+
+    /* ドライバの中で使用するプライベートなデータの準備 */
+    lo = memory_alloc(sizeof(*lo));
+    if (!lo)
+    {
+        errorf("memory_alloc() failure");
+        return NULL;
+    }
+    lo->irq = LOOPBACK_IRQ;
+    mutex_init(&lo->mutex);
+    queue_init(&lo->queue);
+    dev->priv = lo; /* プライベートなデータをデバイス構造体に格納する(ドライバの関数が呼び出される際にはデバイス構造体が渡されるのでここから取り出す) */
+
+    /* デバイスの登録 */
+    if (net_device_register(dev) == -1)
+    {
+        errorf("net_device_register() failure");
+        return NULL;
+    }
+    /* 割り込みハンドラの登録 */
+    intr_request_irq(lo->irq, loopback_isr, INTR_IRQ_SHARED, dev->name, dev);
+
+    debugf("initialized, dev=%s", dev->name);
+    return dev;
 }
