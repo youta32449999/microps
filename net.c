166cf8a2864b3d2f109a8eafec73f749943ce2ab
youta <m.youta.kwg@gmail.com>
2024-02-16 16:09:13 +0000

    (step4)到着データの振り分けと受信キューへの挿入を行う関数を実装

--- net.c
+++ net.c
@@ -16,6 +16,10 @@ struct net_protocol
     void (*handler)(const uint8_t *data, size_t len, struct net_device *dev); /* プロトコルの入力関数へのポインタ */
 };
 
+/**
+ * 受信キューのエントリの構造体
+ * 受信データと付随する情報(メタデータ)を格納
+ */
 struct net_protocol_queue_entry
 {
     struct net_device *dev;
@@ -181,6 +185,36 @@ int net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, si
  */
 int net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
+    struct net_protocol *proto;
+    struct net_protocol_queue_entry *entry;
+
+    for (proto = protocols; proto; proto = proto->next)
+    {
+        if (proto->type == type)
+        {
+            /* プロトコルの受信キューにエントリを挿入 */
+            entry = memory_alloc(sizeof(*entry) + len);
+            if (!entry)
+            {
+                errorf("memory_alloc() failure");
+                return -1;
+            }
+            entry->dev = dev;
+            entry->len = len;
+            memcpy(entry->data, data, len);
+            if (!queue_push(&proto->queue, entry))
+            {
+                errorf("queue_push() failure");
+                memory_free(entry);
+                return -1;
+            }
+            debugf("queue pushed (num:%u), dev=%s, type=0x%04x, len=%zu", proto->queue.num, dev->name, type, len);
+            debugdump(data, len);
+            return 0;
+        }
+    }
+
+    /* unsupported protocol (プロトコルが見つからなかったら黙って捨てる) */
     return 0;
 }
 
