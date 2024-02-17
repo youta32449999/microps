ae6f5a0735aa25ae436409958aad71b6efda06a5
youta <m.youta.kwg@gmail.com>
2024-02-17 15:17:36 +0000

    (step7)IPインタフェースの登録と検索を行う関数を実装

--- ip.c
+++ ip.c
@@ -32,7 +32,7 @@ const ip_addr_t IP_ADDR_ANY = 0x00000000;       /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
 /* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
-static struct ip_iface *ifaces;
+static struct ip_iface *ifaces; /* 登録されているすべてのIPインタフェースのリスト */
 
 /* IPアドレスを文字列からネットワークバイトオーダーのバイナリ値(ip_addr_t)に変換 */
 int ip_addr_pton(const char *p, ip_addr_t *n)
@@ -142,11 +142,43 @@ ip_iface_alloc(const char *unicast, const char *netmask)
 /* NOTE: must not be call after net_run() */
 int ip_iface_register(struct net_device *dev, struct ip_iface *iface)
 {
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
+    char addr3[IP_ADDR_STR_LEN];
+
+    /* IPインタフェースの登録 */
+    if (net_device_add_iface(dev, NET_IFACE(iface)) == -1)
+    {
+        errorf("net_device_add_iface() failure");
+        return -1;
+    }
+
+    /* IOインタフェースのリストの先頭にifaceを挿入する */
+    iface->next = ifaces;
+    ifaces = iface;
+
+    infof("registered: dev=%s, unicast=%s, netmask=%s, broadcast=%s",
+          dev->name,
+          ip_addr_ntop(iface->unicast, addr1, sizeof(addr1)),
+          ip_addr_ntop(iface->netmask, addr2, sizeof(addr1)),
+          ip_addr_ntop(iface->broadcast, addr3, sizeof(addr3)));
+    return 0;
 }
 
+/* 引数addrで指定されたIPアドレスを持つインタフェースを返す */
 struct ip_iface *
 ip_iface_select(ip_addr_t addr)
 {
+    struct ip_iface *entry;
+
+    for (entry = ifaces; entry; entry = entry->next)
+    {
+        if (entry->unicast == addr)
+        {
+            break;
+        }
+    }
+    return entry;
 }
 
 static void
