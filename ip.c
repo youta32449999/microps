49177a1b04efab8df68b5a1986070a26aeda5676
youta <m.youta.kwg@gmail.com>
2024-02-17 07:16:35 +0000

    (step6)IPヘッダ構造体についてコメントを追加

--- ip.c
+++ ip.c
@@ -7,24 +7,29 @@
 #include "net.h"
 #include "ip.h"
 
+/**
+ * IPヘッダを表現するための構造体
+ * この構造体のポインタにキャストすることでバイト列をIPヘッダとみなしてアクセスできる
+ */
 struct ip_hdr
 {
-    uint8_t vhl;
+    uint8_t vhl; /* バージョン(4bit)とIPヘッダ長(4bit)をまとめて8bitとして扱う */
     uint8_t tos;
     uint16_t total;
     uint16_t id;
-    uint16_t offset;
+    uint16_t offset; /* フラグ(3bit)とフラグメントオフセット(13bit)をまとめて16bitとして扱う */
     uint8_t ttl;
     uint8_t protocol;
     uint16_t sum;
     ip_addr_t src;
     ip_addr_t dst;
-    uint8_t options[];
+    uint8_t options[]; /* オプション(可変長なのでフレキシブル配列メンバとする) */
 };
 
 const ip_addr_t IP_ADDR_ANY = 0x00000000;       /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
+/* IPアドレスを文字列からネットワークバイトオーダーのバイナリ値(ip_addr_t)に変換 */
 int ip_addr_pton(const char *p, ip_addr_t *n)
 {
     char *sp, *ep;
@@ -53,6 +58,7 @@ int ip_addr_pton(const char *p, ip_addr_t *n)
     return 0;
 }
 
+/* IPアドレスをネットワークバイトオーダーのバイナリ値(ip_addr_t)から文字列に変換 */
 char *
 ip_addr_ntop(ip_addr_t n, char *p, size_t size)
 {
