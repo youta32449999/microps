9f7778fbbcfe11024a2afb5144f5f70cbe3493c2
youta <m.youta.kwg@gmail.com>
2024-02-17 13:56:41 +0000

    (step7)デバイスとインタフェースの紐付けを行う関数を実装

--- net.c
+++ net.c
@@ -120,11 +120,45 @@ net_device_close(struct net_device *dev)
 /* NOTE: must not be call after net_run() */
 int net_device_add_iface(struct net_device *dev, struct net_iface *iface)
 {
+    struct net_iface *entry;
+
+    /* 重複登録のチェック
+     * 単純化のために1つのデバイスに対して同じfamilyのインタフェースを複数登録できないよう制限している
+     * 登録しようとしているインタフェースと同いfamilyのインタフェースがすでに存在していたらエラーを返す
+     */
+    for (entry = dev->ifaces; entry; entry = entry->next)
+    {
+        if (entry->family == iface->family)
+        {
+            /* NOTE: For simplicity, only one iface can be added per family. */
+            errorf("already exists, dev=%s, family=%d", dev->name, entry->family);
+            return -1;
+        }
+    }
+    /* デバイスとインタフェースを紐づける */
+    iface->dev = dev;
+
+    /* デバイスのインタフェースリストの先頭にifaceを挿入 */
+    iface->next = dev->ifaces;
+    dev->ifaces = iface;
+
+    return 0;
 }
 
+/* デバイスに紐づくインタフェースを検索 */
 struct net_iface *
 net_device_get_iface(struct net_device *dev, int family)
 {
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next)
+    {
+        if (entry->family == family)
+        {
+            break;
+        }
+    }
+    return entry;
 }
 
 int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
