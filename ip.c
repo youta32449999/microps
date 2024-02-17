8366d79a91bec040bea85fdae2bd5078140b48f6
youta <m.youta.kwg@gmail.com>
2024-02-17 07:31:29 +0000

    (step6)デバッグ出力を行う関数を実装

--- ip.c
+++ ip.c
@@ -72,6 +72,32 @@ ip_addr_ntop(ip_addr_t n, char *p, size_t size)
 static void
 ip_dump(const uint8_t *data, size_t len)
 {
+    struct ip_hdr *hdr;
+    uint8_t v, hl, hlen;
+    uint16_t total, offset;
+    char addr[IP_ADDR_STR_LEN];
+
+    flockfile(stderr);
+    hdr = (struct ip_hdr *)data;
+    v = (hdr->vhl & 0xf0) >> 4; /* vhlの上位4bit=バージョン */
+    hl = hdr->vhl & 0x0f;       /* vhlの下位4bit=IPヘッダ長 */
+    hlen = hl << 2;             /* IPヘッダ長は32bit(4byte)単位の値が格納されているので4倍して8bit(1byte)単位の値にする */
+    fprintf(stderr, "        vhl: 0x%02x [v: %u, hl: %u (%u)]\n", hdr->vhl, v, hl, hlen);
+    fprintf(stderr, "        tos: 0x%02x\n", hdr->tos);
+    total = ntoh16(hdr->total);                                              /* 多バイト長(16bitや32bit)の数値データはバイトオーダーの変換が必要 */
+    fprintf(stderr, "      total: %u (payload: %u)\n", total, total - hlen); /* トータル長からIPヘッダ長を引いたものが運んでいるデータ(ペイロード)の長さ */
+    fprintf(stderr, "         id: %u\n", ntoh16(hdr->id));
+    offset = ntoh16(hdr->offset);
+    fprintf(stderr, "     offset: 0x%04x [flags=%x, offset=%u]\n", offset, (offset & 0xe000) >> 13, offset & 0x1fff); /* offsetの上位3bit = フラグ, 下位13bit = フラグメントオフセット */
+    fprintf(stderr, "        ttl: %u\n", hdr->ttl);
+    fprintf(stderr, "   protocol: %u\n", hdr->protocol);
+    fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+    fprintf(stderr, "        src: %s\n", ip_addr_ntop(hdr->src, addr, sizeof(addr))); /* IPアドレスをネットワークバイトオーダーのバイナリ値(ip_addr_t)から文字列に変換 */
+    fprintf(stderr, "        dst: %s\n", ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
 }
 
 static void
