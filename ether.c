11cd61058aea3b87ef50c7438d190bd291738a49
youta <m.youta.kwg@gmail.com>
2024-02-20 13:07:45 +0000

    (step12)Ethernetヘッダのデバッグ出力関数を実装

--- ether.c
+++ ether.c
@@ -64,6 +64,18 @@ ether_addr_ntop(const uint8_t *n, char *p, size_t size)
 static void
 ether_dump(const uint8_t *frame, size_t flen)
 {
+    struct ether_hdr *hdr;
+    char addr[ETHER_ADDR_STR_LEN];
+
+    hdr = (struct ether_hdr *)frame;
+    flockfile(stderr);
+    fprintf(stderr, "        src: %s\n", ether_addr_ntop(hdr->src, addr, sizeof(addr)));
+    fprintf(stderr, "        dst: %s\n", ether_addr_ntop(hdr->dst, addr, sizeof(addr)));
+    fprintf(stderr, "       type: 0x%04x\n", ntoh16(hdr->type));
+#ifdef HEXDUMP
+    hexdump(stderr, frame, flen);
+#endif
+    funlockfile(stderr);
 }
 
 int ether_transmit_helper(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst, ether_transmit_func_t callback)
