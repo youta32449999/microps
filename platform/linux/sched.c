325ce116df73cfcf685f7832fc4320c86483630e
youta <m.youta.kwg@gmail.com>
2024-02-25 17:01:57 +0000

    (step21)タスクの休止と起床を行う関数を実装

--- platform/linux/sched.c
+++ platform/linux/sched.c
@@ -21,12 +21,53 @@ int sched_ctx_destroy(struct sched_ctx *ctx)
 
 int sched_sleep(struct sched_ctx *ctx, mutex_t *mutex, const struct timespec *abstime)
 {
+    int ret;
+
+    /* interruptedのフラグが立っていたらerrnoにEINTRを設定してエラーを返す */
+    if (ctx->interrupted)
+    {
+        errno = EINTR;
+        return -1;
+    }
+    ctx->wc++; /* waitカウントをインクリメント */
+    /*
+     * pthread_cond_broadcast()が呼ばれるまでスレッドを休止させる
+     * asbtimeが指定されていたら指定時刻に起床するpthread_cond_timedwait()を使用する
+     * ※ 休止する際にはmutexがアンロックされ、起床する際にロックされた状態で戻ってくる
+     */
+    if (abstime)
+    {
+        ret = pthread_cond_timedwait(&ctx->cond, mutex, abstime);
+    }
+    else
+    {
+        ret = pthread_cond_wait(&ctx->cond, mutex);
+    }
+    ctx->wc--; /* waitカウントをデクリメント */
+    if (ctx->interrupted)
+    {
+        /* 休止中だったスレッドがすべて起床したらinterruptedフラグを下げる */
+        if (!ctx->wc)
+        {
+            ctx->interrupted = 0;
+        }
+
+        /* errnoにEINTRを設定してエラーを返す */
+        errno = EINTR;
+        return -1;
+    }
+    return ret;
 }
 
 int sched_wakeup(struct sched_ctx *ctx)
 {
+    /* 休止しているスレッドを気象させる */
+    return pthread_cond_broadcast(&ctx->cond);
 }
 
 int sched_interrupt(struct sched_ctx *ctx)
 {
+    /* interruptedフラグを立てた上で休止しているスレッドを起床させる */
+    ctx->interrupted = 1;
+    return pthread_cond_broadcast(&ctx->cond);
 }
