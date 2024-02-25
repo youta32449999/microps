159571ed104c54ae1f12a68a8eb80a77b0dd009b
youta <m.youta.kwg@gmail.com>
2024-02-25 16:45:48 +0000

    (step21)タスクスケジュール用構造体の初期化と破棄を行う関数の実装

--- platform/linux/sched.c
+++ platform/linux/sched.c
@@ -6,10 +6,17 @@
 
 int sched_ctx_init(struct sched_ctx *ctx)
 {
+    /* 初期化 */
+    pthread_cond_init(&ctx->cond, NULL);
+    ctx->interrupted = 0;
+    ctx->wc = 0;
+    return 0;
 }
 
 int sched_ctx_destroy(struct sched_ctx *ctx)
 {
+    /* 条件変数の破棄(待機中のスレッドが存在する場合にのみエラーが返る) */
+    return pthread_cond_destroy(&ctx->cond);
 }
 
 int sched_sleep(struct sched_ctx *ctx, mutex_t *mutex, const struct timespec *abstime)
